fc65818cf1b6c19429fa490a0f58ac2df38992b8
timfewi <123400131+timfewi@users.noreply.github.com>
2026-04-27 15:30:04 +0200

    feat: better git functions (diff, history, ...)

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,6 +2,49 @@ return {
   { 'NMAC427/guess-indent.nvim', opts = {} },
   {
     'lewis6991/gitsigns.nvim',
+    opts = function()
+      return {
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+        word_diff = true,
+        on_attach = function(bufnr)
+          local gitsigns = require 'gitsigns'
+
+          local function map(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+          end
+
+          map('n', ']h', function()
+            if vim.wo.diff then
+              vim.cmd.normal { ']c', bang = true }
+              return
+            end
+
+            gitsigns.nav_hunk 'next'
+          end, 'Next git hunk')
+
+          map('n', '[h', function()
+            if vim.wo.diff then
+              vim.cmd.normal { '[c', bang = true }
+              return
+            end
+
+            gitsigns.nav_hunk 'prev'
+          end, 'Previous git hunk')
+
+          map('n', '<leader>hp', gitsigns.preview_hunk, 'Git [H]unk [P]review')
+          map('n', '<leader>hi', gitsigns.preview_hunk_inline, 'Git hunk [I]nline preview')
+          map('n', '<leader>hd', function()
+            gitsigns.diffthis(nil, { vertical = true })
+          end, 'Git [H]unk [D]iff current file')
+        end,
+      }
+    end,
     keys = {
       {
         '<leader>gt',
@@ -17,14 +60,12 @@ return {
         end,
         desc = '[G]it [T]oggle changes',
       },
-    },
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+      {
+        '<leader>tw',
+        function()
+          require('gitsigns').toggle_word_diff()
+        end,
+        desc = '[T]oggle git [W]ord diff',
       },
     },
   },
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,64 @@
 return {
   {
+    'sindrets/diffview.nvim',
+    cmd = {
+      'DiffviewOpen',
+      'DiffviewClose',
+      'DiffviewToggleFiles',
+      'DiffviewFocusFiles',
+      'DiffviewFileHistory',
+      'DiffviewRefresh',
+      'DiffviewLog',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<CR>', desc = '[G]it [D]iff view' },
+      { '<leader>gD', '<cmd>DiffviewClose<CR>', desc = '[G]it diff close' },
+      { '<leader>gm', '<cmd>DiffviewOpen<CR>', desc = '[G]it [M]erge/conflict view' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory %<CR>', desc = '[G]it file [H]istory' },
+      { '<leader>gH', '<cmd>DiffviewFileHistory<CR>', desc = '[G]it repo history' },
+      { '<leader>ge', '<cmd>DiffviewFocusFiles<CR>', desc = '[G]it diff focus fil[e]s' },
+      { '<leader>gb', '<cmd>DiffviewToggleFiles<CR>', desc = '[G]it diff side[b]ar' },
+      {
+        '<leader>gu',
+        function()
+          require('gitsigns').preview_hunk_inline()
+        end,
+        desc = '[G]it inline diff in c[U]rrent buffer',
+      },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      use_icons = true,
+      view = {
+        default = {
+          layout = 'diff2_horizontal',
+        },
+        merge_tool = {
+          layout = 'diff3_mixed',
+          disable_diagnostics = true,
+        },
+        file_history = {
+          layout = 'diff2_horizontal',
+        },
+      },
+      file_panel = {
+        win_config = {
+          position = 'left',
+          width = 35,
+        },
+      },
+      file_history_panel = {
+        win_config = {
+          position = 'bottom',
+          height = 16,
+        },
+      },
+    },
+  },
+  {
     'kdheepak/lazygit.nvim',
     cmd = {
       'LazyGit',
